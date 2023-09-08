cb6958973ea6ad4a46f85b13bb5e22013012f6d1
MacreenSmith <macreensmith@gmail.com>
2023-09-07 21:12:35 -0400

    Added express, dotenv, port, and a request as a test.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Weather App Running')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/weather', (req, res) => {
+    res.send('Weather info receieved.')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
