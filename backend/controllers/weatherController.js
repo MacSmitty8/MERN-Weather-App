577c911ed6b13efaf8982833eaa97778914b61a1
MacreenSmith <macreensmith@gmail.com>
2023-09-09 17:13:54 -0400

    Changed app use to app get in server js

--- backend/controllers/weatherController.js
+++ backend/controllers/weatherController.js
@@ -1,8 +1,12 @@
+//Pass ZIP code so that when entered the api will return it
+//Get the data points needed for the display on the page.
+// temperture, weather_descriptions, weather_icons, localtime
+
 const asyncHandler = require('express-async-handler')
 //This will get a weather from a user.
 // Route GET /api/weather
 // Access ???
-const getWeather =  asyncHandler(async (req, res) => {
+const view =  asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Weather info receieved.'})
 })
 
@@ -30,7 +34,7 @@ const deleteWeather = async (req, res) => {
     res.status(200).json({message: 'Weather info receieved.'})
 }
 module.exports ={
-    getWeather,
+    view,
     setWeather,
     updateWeather,
     deleteWeather,
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const errorHandler = require('./middleware/errorMiddleware')
+const weatherRoute = require('./routes/weatherRoutes')
 const port = process.env.PORT || 5000
 
 
 const app = express()
 
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // app.use('/api/weather', require('./routes/weatherRoutes'))
 app.use(errorHandler.errorHandler)
+app.get('/weather', weatherRoute.view)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
