051927ddd62347d9e6a80ddf0e393a5751b54b0b
MacreenSmith <macreensmith@gmail.com>
2023-09-11 10:30:35 -0400

    Added a form to the register page.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { FaUser } from "react-icons/fa"
 
 function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
+
+    const {name, email, password, confirmPassword} = formData
+
+    const onChange = () => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <section>
+        <h1>
+            <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+        <section>
+            <form onSubmit={onSubmit}>
+                <input type='text' 
+                id='name' 
+                name='name' 
+                value={name} 
+                placeholder='Enter your name' 
+                onChange={onChange}    
+                />
+                  <input type='text' 
+                id='email' 
+                name='email' 
+                value={email} 
+                placeholder='Enter your email' 
+                onChange={onChange}    
+                />
+                  <input type='text' 
+                id='password' 
+                name='password' 
+                value={password} 
+                placeholder='Enter your password' 
+                onChange={onChange}    
+                />
+                  <input type='text' 
+                id='confirmPassword' 
+                name='confirmPassword' 
+                value={confirmPassword} 
+                placeholder='Please confirm your password' 
+                onChange={onChange}    
+                />
+                <div>
+                    <button type='submit'>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </section>
+      </section>
+    </>
   )
 }
 
