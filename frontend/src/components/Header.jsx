d2ef84dab420cce48e96c60fec3f0c3dc2a3041b
MacreenSmith <macreensmith@gmail.com>
2023-09-11 09:23:25 -0400

    Added the appropriate tages to the Header.jsx Login and Register paths

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,11 @@ function Header() {
         </div> 
         <ul>
             <li>
+                <Link to='/login'>
+                    <FaSignInAlt /> Login
+                </Link>
+            </li>
+            <li>
                 <Link to='/register'>
                     <FaUser /> Register
                 </Link>
